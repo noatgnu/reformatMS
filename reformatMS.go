4c094758fc1e9a8fd15a023350c228cdb0bc16b6
Toan Phung <toan.phung@uqconnect.edu.au>
2018-07-27 20:40:50 +1000

    update

--- reformatMS.go
+++ reformatMS.go
@@ -35,6 +35,20 @@ func main() {
 		"Control should be just the name of the sample, like bio replicate but with the _1.\n" +
 		"The FDR file copied from the PeakView .xslx output file must be saved as .csv - only the FDR sheet.")
 
+	openSWATHfile, openFDRfile, filename := TakeUserInput()
+
+	swathFile := fileHandler.ReadFile(openSWATHfile, 1)
+	samples := len(swathFile.Header) - 9
+	log.Printf("%d Samples", samples)
+	fdrFile := fileHandler.ReadFile(openFDRfile, 1)
+
+	fdrMap := ExtractFDRMap(fdrFile, samples)
+
+	ProcessIons(filename, swathFile, fdrMap, samples)
+	log.Println("Completed.")
+}
+
+func TakeUserInput() (string, string, string) {
 	var openSWATHfile, openFDRfile, filename string
 	var err error
 	openSWATHfile, err = userInput(openSWATHfile, *swath, "What SWATH-MS file are you opening (written like: SWATH.csv): ", err)
@@ -44,34 +58,10 @@ func main() {
 	filename, err = userInput(filename, *out, "What are your output file (written like: output.csv): ", err)
 	filename = input.Clean(filename)
 	log.Printf("Input:\n- SWATH File: %s\n- FDR File: %s\n- Output File: %s ", openSWATHfile, openFDRfile, filename)
+	return openSWATHfile, openFDRfile, filename
+}
 
-	swathFile := fileHandler.ReadFile(openSWATHfile, 1)
-	samples := len(swathFile.Header) - 9
-	log.Printf("%d Samples", samples)
-	fdrFile := fileHandler.ReadFile(openFDRfile, 1)
-
-	fdrMap := make(map[string][]float64)
-	log.Println("Mapping FDR to accession ID.")
-	for c := range fdrFile.OutputChan {
-		fdrFail := 0
-		var fdrArray []float64
-		for i := 0; i < samples; i++ {
-			val, err := strconv.ParseFloat(c[7+i], 64)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			if val >= *threshold {
-				fdrFail++
-			}
-			fdrArray = append(fdrArray, val)
-		}
-		if fdrFail < samples {
-
-			fdrMap[c[1]] = fdrArray
-		}
-	}
-
+func ProcessIons(filename string, swathFile fileHandler.FileObject, fdrMap map[string][]float64, samples int) {
 	o, err := os.Create(filename)
 	if err != nil {
 		log.Fatalln(err)
@@ -95,10 +85,10 @@ func main() {
 					swathSampleMap[swathFile.Header[9+i]] = sample[:]
 				}
 
-				row := fmt.Sprintf("%v,%v,%v,%v,%v,%v,%v,%v,%v,", c[0], c[1], c[3], c[7] + c[8], c[6], "L",
+				row := fmt.Sprintf("%v,%v,%v,%v,%v,%v,%v,%v,%v,", c[0], c[1], c[3], c[7]+c[8], c[6], "L",
 					sample[0],
 					swathFile.Header[9+i],
-					strconv.Itoa(i + 1))
+					strconv.Itoa(i+1))
 				if val[i] < *threshold {
 					row += c[9+i]
 					if c[9+i] == "" {
@@ -116,12 +106,34 @@ func main() {
 				writer.WriteString(temp)
 			}
 		}
-
 	}
-
 	writer.Flush()
 	o.Close()
-	log.Println("Completed.")
+}
+
+func ExtractFDRMap(fdrFile fileHandler.FileObject, samples int) map[string][]float64 {
+	fdrMap := make(map[string][]float64)
+	log.Println("Mapping FDR to accession ID.")
+	for c := range fdrFile.OutputChan {
+		fdrFail := 0
+		var fdrArray []float64
+		for i := 0; i < samples; i++ {
+			val, err := strconv.ParseFloat(c[7+i], 64)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			if val >= *threshold {
+				fdrFail++
+			}
+			fdrArray = append(fdrArray, val)
+		}
+		if fdrFail < samples {
+
+			fdrMap[c[1]] = fdrArray
+		}
+	}
+	return fdrMap
 }
 
 func userInput(openSWATHfile string, arg string, message string, err error) (string, error) {
